152bd382eb194ed90e2f8f10e289ee6fd5598632
kumarprincepk <princekumar35071@gmail.com>
2025-08-31 21:27:16 +0530

    bootstrap css issue fixed

--- components/footerSection/Footer.jsx
+++ components/footerSection/Footer.jsx
@@ -4,8 +4,8 @@ import styles from './FooterSection.module.css';
 
 export default function Footer() {
     return (
-        <footer className={styles.classCall}>
-            <h1>Footer Section</h1>
+        <footer className={`${styles.classCall} bg-dark`}>
+            <h1 className='bg-primary'>Footer Section</h1>
         </footer>
     )
 }
